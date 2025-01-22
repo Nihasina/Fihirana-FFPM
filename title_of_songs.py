e33ade83f392eec16da4793f3d8733359f4b97bf
Nihasina <47098617+Nihasina@users.noreply.github.com>
2025-01-22 09:16:44 +0300

    Update title_of_songs.py

--- title_of_songs.py
+++ title_of_songs.py
@@ -33,3 +33,5 @@ with open(file_path, "w", encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
 
 print("Fichier JSON modifié avec succès.")
+
+#code navoakan'i chatGpt
